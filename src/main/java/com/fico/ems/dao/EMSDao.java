2e485c2c38c4bf74578a566892ba0b5823723300
Mukesh Kumar <MukeshKumar@fico.com>
2018-09-09 12:08:12 +0530

    second commit

--- src/main/java/com/fico/ems/dao/EMSDao.java
+++ src/main/java/com/fico/ems/dao/EMSDao.java
@@ -1,10 +1,10 @@
 package com.fico.ems.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
 
 import com.fico.ems.model.Employee;
-@RepositoryRestResource
+@Repository
 public interface EMSDao extends JpaRepository<Employee, Integer> {
 Employee findByEmail(String email);
 }
